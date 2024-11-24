5f26c7b6d6550262ff5adda92e66f58064236e77
fazelmawlaa <mawlafazel50@gmail.com>
2024-11-24 21:18:59 +0700

    Update main.c

--- main.c
+++ main.c
@@ -107,32 +107,6 @@ int login(const char *username, const char *password) {
     return 0; // Login gagal
 }
 
-// Fungsi untuk menampilkan peraturan
-void rules() {
-    int rule;
-
-    printf("\n===================================================\n");
-    printf("      SELAMAT DATANG DI GAME BRAIN BILLIONARE       \n");
-    printf("===================================================\n");
-
-    printf("Peraturan permainan: \n");
-    printf("1. Terdapat 5 soal yang jumlah hadiahnya berbeda tergantung tingkat kesulitan.\n");
-    printf("2. Tingkat kesulitan dan prizepool:\n");
-    printf("   Easy\t\tRp 500.000\n");
-    printf("   Normal\tRp 800.000\n");
-    printf("   Hard\t\tRp 1.000.000\n");
-    printf("   Very Hard\tRp 2.000.000\n");
-    printf("3. Game akan berakhir jika soal dijawab salah dan uang yang anda kumpulkan akan hangus.\n");
-    printf("\nApakah Anda ingin memulai permainan?\n");
-    printf("1. GAZZZ\n2. SKIP DULU DEH\n\nJawaban Anda: ");
-    scanf("%d", &rule);
-
-    if (rule == 1) {
-        mulaiKuis();
-    } else {
-        printf("Baik, terima kasih sudah berpartisipasi\n");
-    }
-}
 
 // Fungsi untuk menjalankan sistem (program) kuis
 void jalankanKuis(Soal *kuis, int jumlahSoal, int nilaiHadiah) {
@@ -248,6 +222,33 @@ int diff;
             {"Alat untuk mengukur penguapan udara?", {"Barometer", "Atmometer", "Hygrometer", "Mikrometer"}, 'B'}
         };
         jalankanKuis(kuisVeryHard, sizeof(kuisVeryHard) / sizeof(kuisVeryHard[0]), nilaiHadiah);
+    }
+}
+
+// Fungsi untuk menampilkan peraturan
+void rules() {
+    int rule;
+
+    printf("\n===================================================\n");
+    printf("      SELAMAT DATANG DI GAME BRAIN BILLIONARE       \n");
+    printf("===================================================\n");
+
+    printf("Peraturan permainan: \n");
+    printf("1. Terdapat 5 soal yang jumlah hadiahnya berbeda tergantung tingkat kesulitan.\n");
+    printf("2. Tingkat kesulitan dan prizepool:\n");
+    printf("   Easy\t\tRp 500.000\n");
+    printf("   Normal\tRp 800.000\n");
+    printf("   Hard\t\tRp 1.000.000\n");
+    printf("   Very Hard\tRp 2.000.000\n");
+    printf("3. Game akan berakhir jika soal dijawab salah dan uang yang anda kumpulkan akan hangus.\n");
+    printf("\nApakah Anda ingin memulai permainan?\n");
+    printf("1. GAZZZ\n2. SKIP DULU DEH\n\nJawaban Anda: ");
+    scanf("%d", &rule);
+
+    if (rule == 1) {
+        mulaiKuis();
+    } else {
+        printf("Baik, terima kasih sudah berpartisipasi\n");
     }
 }
 
